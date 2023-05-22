48fd517682b719c363e503c8c50dfb5900971e6b
Heavyrisem <heavyrisem@gmail.com>
2023-05-23 01:26:19 +0900

    setting for storybook deploy

--- apps/storybook/storybook-static/assets/es.map.constructor-7becbfe3.js
+++
@@ -1,2 +0,0 @@
-var r=Object.defineProperty;var t=(n,o)=>r(n,"name",{value:o,configurable:!0});import{ao as e,ap as a}from"./iframe-c3cf687c.js";var c=e,i=a;c("Map",function(n){return t(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},i);
-//# sourceMappingURL=es.map.constructor-7becbfe3.js.map
--- apps/storybook/storybook-static/assets/preview-aa09d43a.js
+++
@@ -1,2 +0,0 @@
-import{j as r,p as e}from"./Skeleton-d01f91ea.js";import"./index-4ec46995.js";import"./iframe-c3cf687c.js";const n={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},cssUserPrefs:{"prefers-color-scheme":"dark"}},i=[(o,s)=>r.jsx(e,{children:r.jsx(o,{...s})})];export{i as decorators,n as parameters};
-//# sourceMappingURL=preview-aa09d43a.js.map
