11bcd5d16aef689bce9d48eec842a9821fc97870
Heavyrisem <Heavyrisem@users.noreply.github.com>
2024-02-03 15:03:12 +0000

    deploy: 3613ce8cf1084f3ca76ae2e20723417d917ab635

---
+++ assets/Button.stories-0FbyJ6UK.js
@@ -0,0 +1,30 @@
+import{G as v}from"./index-h_7pF1Ko.js";import"./jsx-runtime-AgcCsxC8.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";const P={title:"Example/Button",component:v,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},r={args:{children:"Button"}},e={args:{children:"Button",variant:"outlined"}},s={args:{children:"Button",disabled:!0}},a={args:{children:"Button",size:"sm"}},t={args:{children:"Button",size:"md"}},n={args:{children:"Button",size:"lg"}};var o,c,d;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Button'
+  }
+}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var i,m,u;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    variant: 'outlined'
+  }
+}`,...(u=(m=e.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var l,p,g;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    disabled: true
+  }
+}`,...(g=(p=s.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var B,h,S;a.parameters={...a.parameters,docs:{...(B=a.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    size: 'sm'
+  }
+}`,...(S=(h=a.parameters)==null?void 0:h.docs)==null?void 0:S.source}}};var z,b,y;t.parameters={...t.parameters,docs:{...(z=t.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    size: 'md'
+  }
+}`,...(y=(b=t.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var x,O,f;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    size: 'lg'
+  }
+}`,...(f=(O=n.parameters)==null?void 0:O.docs)==null?void 0:f.source}}};const _=["Primary","Outlined","Disabled","Small","Medium","Large"];export{s as Disabled,n as Large,t as Medium,e as Outlined,r as Primary,a as Small,_ as __namedExportsOrder,P as default};
---
+++ assets/Tag.stories-JZN2v8j4.js
@@ -0,0 +1,23 @@
+import{j as o}from"./jsx-runtime-AgcCsxC8.js";import{J as t}from"./index-h_7pF1Ko.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";const j={title:"Example/Tag",component:t,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{children:"Tag"}},s={render:n=>o.jsx("div",{style:{display:"flex",gap:"10px"},children:["default","red","green","blue"].map(e=>o.jsx(t,{...n,color:e,children:e}))})},a={render:n=>o.jsx("div",{style:{display:"flex",gap:"10px"},children:["sm","md","lg"].map(e=>o.jsx(t,{...n,size:e,children:e}))})};var d,p,c;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: 'Tag'
+  }
+}`,...(c=(p=r.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var l,i,m;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    gap: '10px'
+  }}>
+      {(['default', 'red', 'green', 'blue'] as const).map(color => <Tag {...args} color={color}>
+          {color}
+        </Tag>)}
+    </div>
+}`,...(m=(i=s.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var g,u,x;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    gap: '10px'
+  }}>
+      {(['sm', 'md', 'lg'] as const).map(size => <Tag {...args} size={size}>
+          {size}
+        </Tag>)}
+    </div>
+}`,...(x=(u=a.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};const v=["Primary","Colors","Sizes"];export{s as Colors,r as Primary,a as Sizes,v as __namedExportsOrder,j as default};
