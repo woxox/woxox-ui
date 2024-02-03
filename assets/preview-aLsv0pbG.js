aafddd0c5192628b2a31650fbfb87ad1d5e939c9
Heavyrisem <Heavyrisem@users.noreply.github.com>
2024-02-03 03:31:07 +0000

    deploy: e9fd4aef20a284a212f59ec34b019337c18a4510

---
+++ assets/preview-aLsv0pbG.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-jfcKhe_B.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-ED5BO1wg.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-ED5BO1wg.js","./iframe-jfcKhe_B.js","./index-XiNr8FW2.js","./_commonjsHelpers-5-cIlDoe.js","./react-18-sFOusbzT.js","./index-9vG4XYWr.js","./index-4aa2jg4c.js","./index-ogXoivrg.js","./index-mLPG47JP.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
