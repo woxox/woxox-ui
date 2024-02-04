084189f71be713bc9692455b0c3dbf9de81d8ebd
WhiteKr <hanjisu2215g@gmail.com>
2024-02-04 20:04:18 +0900

    [#14] feat: Input component
    - added options label, value and placeholder.
    - updated style better

--- apps/storybook/src/stories/Input.stories.tsx
+++ apps/storybook/src/stories/Input.stories.tsx
@@ -12,10 +12,15 @@ const meta: Meta<typeof Input> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: { label: 'Label' },
+};
 
 export const Outlined: Story = {
-  args: { variant: 'outlined' },
+  args: {
+    label: 'Label',
+    variant: 'outlined',
+  },
 };
 
 // export const Underlined: Story = {
@@ -23,14 +28,24 @@ export const Outlined: Story = {
 // };
 
 export const Disabled: Story = {
-  args: { disabled: true },
+  args: {
+    label: 'Label',
+    disabled: true,
+  },
+};
+
+export const Placeholder: Story = {
+  args: {
+    label: 'Label',
+    placeholder: 'Placeholder',
+  },
 };
 
 export const Sizes: Story = {
   render: (args) => (
     <div style={{ display: 'flex', gap: '10px' }}>
       {(['sm', 'md', 'lg'] as const).map((size) => (
-        <Input {...args} size={size} />
+        <Input {...args} label="Label" size={size} />
       ))}
     </div>
   ),
--- packages/woxox-ui/src/components/atoms/Input/index.tsx
+++ packages/woxox-ui/src/components/atoms/Input/index.tsx
@@ -1,27 +1,48 @@
 import { RecipeVariants } from '@vanilla-extract/recipes';
 import clsx from 'clsx';
 
-import { inputVariants } from './input.css';
+import { Text } from '@/components/atoms/Text';
+import { baseToken, colorToken } from '@/themes/theme.css';
 
-export type InputProps = React.HTMLAttributes<HTMLInputElement> &
-  RecipeVariants<typeof inputVariants>;
+import { inputVariants } from './input.css';
+import { COLOR_PALETTE } from '@/themes/tokens/color-palette';
+
+export type InputProps = React.HTMLAttributes<HTMLInputElement> & {
+  label: string;
+  value: string | number | readonly string[] | undefined;
+  placeholder: string | undefined;
+} & RecipeVariants<typeof inputVariants>;
 
 const Input = ({
   variant,
   disabled,
   size,
   className,
+  label,
+  value,
+  placeholder,
   ...rest
 }: InputProps) => {
   return (
-    <input
-      className={clsx(
-        inputVariants({ variant, disabled, size }),
-        className,
-      )}
-      disabled={disabled}
-      {...rest}
-    />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: baseToken.spacing['0.5'],
+      }}
+    >
+      <Text style={{ ...baseToken.fontSize[12] }}>{label}</Text>
+      <input
+        className={clsx(
+          inputVariants({ variant, disabled, size }),
+          className,
+        )}
+        disabled={disabled}
+        value={value}
+        placeholder={placeholder}
+        {...rest}
+      />
+    </div>
   );
 };
 
