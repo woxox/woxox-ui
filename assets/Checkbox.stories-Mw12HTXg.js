9495f6f21fc4eb514df4b125ff34b2d0de1fd3ae
Heavyrisem <Heavyrisem@users.noreply.github.com>
2024-02-11 12:05:11 +0000

    deploy: 179da0c28b87d79e50dc740140ba02890d2e01f8

---
+++ assets/Checkbox.stories-Mw12HTXg.js
@@ -0,0 +1,11 @@
+import{E as i}from"./index-zkaG0te7.js";import"./jsx-runtime-AgcCsxC8.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";const h={title:"Example/Checkbox",component:i,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},e={args:{}},r={args:{disabled:!0}},a={args:{defaultChecked:!0}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var c,d,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var n,p,u;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    defaultChecked: true
+  }
+}`,...(u=(p=a.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};const k=["Primary","Disabled","DefaultChecked"];export{a as DefaultChecked,r as Disabled,e as Primary,k as __namedExportsOrder,h as default};
