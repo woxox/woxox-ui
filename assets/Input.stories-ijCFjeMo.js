9813bd41b234227c14b40826265a2a9ae08bf473
Heavyrisem <Heavyrisem@users.noreply.github.com>
2024-02-11 11:43:24 +0000

    deploy: 27841fe3d4ae2b753f746598aa07816b6dda07ee

---
+++ assets/Input.stories-ijCFjeMo.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-AgcCsxC8.js";import{V as j}from"./index-_exp-cod.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";const _={title:"Example/Input",component:j,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{label:"Label"}},a={args:{label:"Label",variant:"outlined"}},r={args:{label:"Label",variant:"underlined"}},s={args:{label:"Label",disabled:!0}},l={args:{label:"Label",placeholder:"Placeholder"}},o={render:E=>n.jsx("div",{style:{display:"flex",gap:"10px"},children:["sm","md","lg"].map(O=>n.jsx(j,{...E,label:"Label",size:O}))})};var t,d,c;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Label'
+  }
+}`,...(c=(d=e.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var i,p,m;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    variant: 'outlined'
+  }
+}`,...(m=(p=a.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var u,b,g;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    variant: 'underlined'
+  }
+}`,...(g=(b=r.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var L,x,S;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    disabled: true
+  }
+}`,...(S=(x=s.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};var h,v,y;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    placeholder: 'Placeholder'
+  }
+}`,...(y=(v=l.parameters)==null?void 0:v.docs)==null?void 0:y.source}}};var P,f,z;o.parameters={...o.parameters,docs:{...(P=o.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    gap: '10px'
+  }}>
+      {(['sm', 'md', 'lg'] as const).map(size => <Input {...args} label="Label" size={size} />)}
+    </div>
+}`,...(z=(f=o.parameters)==null?void 0:f.docs)==null?void 0:z.source}}};const R=["Primary","Outlined","Underlined","Disabled","Placeholder","Sizes"];export{s as Disabled,a as Outlined,l as Placeholder,e as Primary,o as Sizes,r as Underlined,R as __namedExportsOrder,_ as default};
---
+++ assets/Text.stories-MQqCmNHf.js
@@ -0,0 +1,5 @@
+import{b as o}from"./index-_exp-cod.js";import"./jsx-runtime-AgcCsxC8.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";const p={title:"Example/Text",component:o,tags:["autodocs"],parameters:{layout:"centered"}},r={args:{children:"text"}};var e,t,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    children: 'text'
+  }
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const i=["Primary"];export{r as Primary,i as __namedExportsOrder,p as default};
